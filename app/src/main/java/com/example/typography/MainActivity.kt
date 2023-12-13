7c914f7a2f2a6c18d98012d416ae86a49015277f
Darran Kelinske <darran.kelinske@gmail.com>
2023-12-13 14:03:06 -0600

    compare 3 variants

--- app/src/main/java/com/example/typography/MainActivity.kt
+++ app/src/main/java/com/example/typography/MainActivity.kt
@@ -21,17 +21,56 @@ import androidx.compose.ui.text.font.FontWeight
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val testText = "a"
         setContent {
             Row {
+                MaterialTheme {
+                    Column {
+                        Text(
+                            testText,
+                            style = MaterialTheme.typography.headlineLarge.copy(
+                                //                                fontSynthesis = FontSynthesis.None
+                            ),
+                            maxLines = 1,
+                        )
+                        //                        Text("headlineMedium", style = MaterialTheme.typography.headlineMedium, maxLines = 1)
+                        //                        Text("headlineSmall", style = MaterialTheme.typography.headlineSmall, maxLines = 1)
+                        //                        Text("bodyLarge", style = MaterialTheme.typography.bodyLarge)
+                        //                        Text("bodyMedium", style = MaterialTheme.typography.bodyMedium)
+                        //                        Text("bodySmall", style = MaterialTheme.typography.bodySmall)
+                    }
+                }
+
+                MaterialTheme(
+                    typography = proximaTypography(),
+                ) {
+                    Column {
+                        Text(
+                            testText,
+                            style = MaterialTheme.typography.headlineLarge.copy(
+//                                fontSynthesis = FontSynthesis.None
+                            ),
+                            maxLines = 1,
+                        )
+//                        Text("headlineMedium", style = MaterialTheme.typography.headlineMedium, maxLines = 1)
+//                        Text("headlineSmall", style = MaterialTheme.typography.headlineSmall, maxLines = 1)
+//                        Text("bodyLarge", style = MaterialTheme.typography.bodyLarge)
+//                        Text("bodyMedium", style = MaterialTheme.typography.bodyMedium)
+//                        Text("bodySmall", style = MaterialTheme.typography.bodySmall)
+                    }
+                }
+
                 MaterialTheme(
                     typography = robotoTypography(),
                 ) {
                     Column {
-                        Text("b",
+                        Text(
+                            testText,
                             style = MaterialTheme.typography.headlineLarge.copy(
 //                                fontSynthesis = FontSynthesis.None
                             ),
-                            maxLines = 1
+                            maxLines = 1,
                         )
 //                        Text("headlineMedium", style = MaterialTheme.typography.headlineMedium, maxLines = 1)
 //                        Text("headlineSmall", style = MaterialTheme.typography.headlineSmall, maxLines = 1)
@@ -40,28 +79,235 @@ class MainActivity : ComponentActivity() {
 //                        Text("bodySmall", style = MaterialTheme.typography.bodySmall)
                     }
                 }
-
-                MaterialTheme {
-                    Column {
-                        Text("b",
-                            style = MaterialTheme.typography.headlineLarge.copy(
-//                                fontSynthesis = FontSynthesis.None
-                            ),
-                            maxLines = 1
-                        )
-//                        Text("headlineMedium", style = MaterialTheme.typography.headlineMedium, maxLines = 1)
-//                        Text("headlineSmall", style = MaterialTheme.typography.headlineSmall, maxLines = 1)
-//                        Text("bodyLarge", style = MaterialTheme.typography.bodyLarge)
-//                        Text("bodyMedium", style = MaterialTheme.typography.bodyMedium)
-//                        Text("bodySmall", style = MaterialTheme.typography.bodySmall)
-                    }
-                }
-
-
             }
         }
     }
 }
+
+private val proxima = FontFamily(
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W100,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(0f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W100.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W200,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(0f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W200.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W300,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(0f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W300.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W400,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(0f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W400.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W500,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(0f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W500.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W600,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(0f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W600.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W700,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(0f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W700.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W800,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(0f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W800.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W900,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(0f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W900.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W100,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(1f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W100.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W200,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(1f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W200.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W300,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(1f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W300.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W400,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(1f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W400.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W500,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(1f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W500.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W600,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(1f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W600.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W700,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(1f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W700.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W800,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(1f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W800.weight),
+        ),
+    ),
+    Font(
+        R.font.proximavara,
+        weight = FontWeight.W900,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(
+            FontVariation.italic(1f),
+            FontVariation.width(100f),
+            FontVariation.weight(FontWeight.W900.weight),
+        ),
+    ),
+
+)
+
+// https://issuetracker.google.com/issues/270111243
+@Composable
+fun proximaTypography() = Typography(
+    displayLarge = MaterialTheme.typography.displayLarge.copy(
+        fontFamily = proxima,
+    ),
+    displayMedium = MaterialTheme.typography.displayMedium.copy(
+        fontFamily = proxima,
+    ),
+    displaySmall = MaterialTheme.typography.displaySmall.copy(
+        fontFamily = proxima,
+    ),
+    headlineLarge = MaterialTheme.typography.headlineLarge.copy(
+        fontFamily = proxima,
+    ),
+    headlineMedium = MaterialTheme.typography.headlineMedium.copy(
+        fontFamily = proxima,
+    ),
+    headlineSmall = MaterialTheme.typography.headlineSmall.copy(
+        fontFamily = proxima,
+    ),
+    titleLarge = MaterialTheme.typography.titleLarge.copy(
+        fontFamily = proxima,
+    ),
+    titleMedium = MaterialTheme.typography.titleMedium.copy(
+        fontFamily = proxima,
+    ),
+    titleSmall = MaterialTheme.typography.titleSmall.copy(
+        fontFamily = proxima,
+    ),
+    bodyLarge = MaterialTheme.typography.bodyLarge.copy(
+        fontFamily = proxima,
+    ),
+    bodyMedium = MaterialTheme.typography.bodyMedium.copy(
+        fontFamily = proxima,
+    ),
+    bodySmall = MaterialTheme.typography.bodySmall.copy(
+        fontFamily = proxima,
+    ),
+    labelLarge = MaterialTheme.typography.labelLarge.copy(
+        fontFamily = proxima,
+    ),
+    labelMedium = MaterialTheme.typography.labelMedium.copy(
+        fontFamily = proxima,
+    ),
+    labelSmall = MaterialTheme.typography.labelSmall.copy(
+        fontFamily = proxima,
+    ),
+)
 
 private val robotoFlex = FontFamily(
     Font(
